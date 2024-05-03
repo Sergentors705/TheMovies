1fe9f864c37ce93e01d55c1b95351d1b4f988e03
Системный Администратор <sergentors705@gmail.com>
2024-05-03 17:28:59 +0600

    wip date filter

--- src/pages/top-rated-page.jsx
+++ src/pages/top-rated-page.jsx
@@ -21,7 +21,7 @@ export default function TopRatedMovies() {
   useAnimationOffsetEffect(embla, 200);
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=vote_average.desc&vote_average.gte=${minRating}&vote_average.lte=${maxRating}&without_genres=99,10755&vote_count.gte=1000`, {
+    fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=vote_average.desc&vote_average.gte=${minRating}&vote_average.lte=${maxRating}&without_genres=99,10755&vote_count.gte=1000&release_date.gte=${minYear.getFullYear()}-${minYear.getMonth()}-${minYear.getDate()}&release_date.lte=${maxYear.getFullYear()}-${maxYear.getMonth()}-${maxYear.getDate()}`, {
       headers: {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTMzZmZiMGJiZDYyMmYxNWEyYzk2ZGI1N2JiNDk5NSIsInN1YiI6IjY1NjYwNGY3ZDk1NDIwMDBmZTMzNDBmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EP_uOQGwm3MJDqxGnJSkPjAXSlGfO6jJU2UbB7GWADc',
         Accept: 'application/json',
@@ -33,14 +33,14 @@ export default function TopRatedMovies() {
       }
     })
     .then((object) => {
-      console.log(object);
+      console.log('ti pidor typoi')
       setPopular(object);
       setLoading(false);
     })
   },[minRating, maxRating, minYear, maxYear, page])
 
   if (loading) return <Loading />;
-  console.log(popular)
+  
   return (
     <Container size={1366}>
       <Title order={1} mb={'md'}>Top rated movies</Title>
@@ -83,18 +83,19 @@ export default function TopRatedMovies() {
             <DatePickerInput
               clearable
               value={minYear}
-              onChange={setMinYear}
+              onChange={value => setMinYear(value)}
             />
             <DatePickerInput
               clearable
               value={maxYear}
-              onChange={setMaxYear}
+              onChange={value => setMaxYear(value)}
             />
           </Box>
         </Paper>
         <Flex wrap={'wrap'} gap={20}>
           {popular?.results?.map((item) =>
             <Box
+              key={item.id}
               maw={220}
               onClick={() => navigate(`/movie/${item.id}`)}
             >
@@ -108,6 +109,7 @@ export default function TopRatedMovies() {
                 order={2}
                 textWrap='wrap'
               >{item.title}</Title>
+              <Text>{item.release_date}</Text>
             </Box>
           )}
         </Flex>
